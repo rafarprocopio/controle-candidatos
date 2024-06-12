3668f04b4e4ca1c4150c293b9995699f085ee8eb
Rafaely RP <rafarprocopio@gmail.com>
2024-06-12 15:18:49 -0300

    Aalteração Prodjeto sistema de validação - processo seletivo

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -2,9 +2,19 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         selecaoCandidatos();
+        imprimirSelecionados();
         analisarCandidato(1850.0);
         analisarCandidato(2300.00);
         analisarCandidato(2000.0);
+
+    }
+
+    static void imprimirSelecionados () {
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+        System.out.println("imprimindo a lista de candidatos informando o indice dod elemento");
+        for(int indice=0; indice < candidatos.length;indice++) {
+            System.out.println("O candidato de nº " + (indice+1) + "é o" + candidatos[indice]);
+        }
 
     }
     static void selecaoCandidatos() {
@@ -26,7 +36,7 @@ public class ProcessoSeletivo {
     }        
     static double valorPretendido() {
      return ThreadLocalRandom.current().nextDouble(1800, 2200);
-}
+    }
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
         if (salarioBase > salarioPretendido) {
