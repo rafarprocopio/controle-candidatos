2931378ee8480bfb24e2993d342f478ab001c830
Rafaely RP <rafarprocopio@gmail.com>
2024-06-12 05:57:07 -0300

    Projeto trilha java - alterações Projeto Sistema de validação de candidatos para processo seletivo

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,5 +1,6 @@
 public class ProcessoSeletivo {
     public static void main(String[] args) {
+        selecaoCandidatos();
         analisarCandidato(1850.0);
         analisarCandidato(2300.00);
         analisarCandidato(2000.0);
@@ -8,10 +9,24 @@ public class ProcessoSeletivo {
     static void selecaoCandidatos() {
         String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
         int candidatosSelecionados = 0;
-        int candidatoAtual = 0;
+        int candidatosAtual = 0;
+        double salarioBase=2000.0
         while(candidatosSelecionados < 5) {
+            String candidato = candidatos[candidatosAtual]; 
+            double salarioPretendido = valorPretendido();
 
+            System.out.println("O candidato" + candidato " Solicitou este valor de salário " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato" + candidato + "foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;         
         }
+    }
+
+    private static double valorPretendido() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'valorPretendido'");
     }
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
