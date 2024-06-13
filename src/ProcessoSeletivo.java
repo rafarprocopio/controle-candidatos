e1367081a9d39c5ad983ea4747a2491dcbe55155
Rafaely RP <rafarprocopio@gmail.com>
2024-06-13 15:09:28 -0300

    Alterações Projeto Java - jSistema de validação processo seletivo.

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,12 +1,41 @@
 import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+        for(String candidato: candidatos) {
+			entrandoEmContato(candidato);
+		}
         selecaoCandidatos();
         imprimirSelecionados();
         analisarCandidato(1850.0);
         analisarCandidato(2300.00);
         analisarCandidato(2000.0);
+    }
+    static void entrandoEmContato(String candidato) {
+            int tentativasRealizadas = 1;
+            boolean continuarTentando = true;
+            boolean atendeu=false;
+            do{
+                atendeu= atender();
+                continuarTentando = !atendeu;
+                if(continuarTentando)
+                    tentativasRealizadas++;
+                else
+                    System.out.println("CONTATO REALIZADO COM SUCESSO");
+                
+            }while(continuarTentando && tentativasRealizadas<3);
+            
+            if(atendeu)
+                System.out.println("CONSEGUIMOS CONTATO COM " + candidato +" NA " + tentativasRealizadas + " TENTATIVA");
+            else
+                System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato +", NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas + " REALIZADA");
+                        
+        }
 
+        static boolean atender() {
+            return new Random().nextInt(3)==1;	
+        }
+        
     }
 
     static void imprimirSelecionados () {
